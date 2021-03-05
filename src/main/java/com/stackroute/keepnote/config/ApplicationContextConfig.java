d43da92cd4ae29dcfa035a8fb1f308325e65c5ab
Chirag Varshney <chirag.varshney@ibm.com>
2021-03-05 16:23:50 +0530

    Third commit

--- src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
@@ -48,11 +48,11 @@ public class ApplicationContextConfig {
 	public DataSource getDataSource() {
 		
 		  BasicDataSource dataSource = new BasicDataSource();
-		  dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); dataSource.setUrl(
-		  "jdbc:mysql://localhost:3306/keepnote2DB?createDatabaseIfNotExist=true&" +
-		  "verifyServerCertificate=false&useSSL=false&requireSSL=false");
-		  dataSource.setUsername("root"); 
-		  dataSource.setPassword("Chirag@0297"); 
+		  dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+			dataSource.setUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":3306/" + System.getenv("MYSQL_DATABASE")
+					+"?verifyServerCertificate=false&useSSL=false&requireSSL=false");
+			dataSource.setUsername(System.getenv("MYSQL_USER"));
+			dataSource.setPassword(System.getenv("MYSQL_PASSWORD")); 
 		  return dataSource;
 		 
 		
